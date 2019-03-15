76d0ea7ada1cf4842bef7622bb33359def2256c2
sabas1080 <s@theinventorhouse.org>
2019-03-14 19:34:14 -0600

    init catwan usb stick

---
+++ libs/electroniccats-catwan-usb-stick/README.md
@@ -0,0 +1,7 @@
+# Electronic Cats CatWAN USB Stick
+
+
+An Open Source USB Stick compatible with LoRa and LoRaWAN with Cortex M0+. To connect, review and analyze packages of LoRa®
+
+
+[More info](https://github.com/ElectronicCats/CatWAN_USB_Stick)
---
+++ libs/electroniccats-catwan-usb-stick/config.ts
@@ -0,0 +1,21 @@
+namespace config {
+
+    export const PIN_D0 = DAL.PA04; //DIO0
+    export const PIN_D1 = DAL.PA23; //DIO1
+    export const PIN_D2 = DAL.PB27; //DIO2
+    export const PIN_D3 = DAL.PA00;
+    export const PIN_D4 = DAL.PA01; 
+    export const PIN_D5 = DAL.PA30;
+    export const PIN_D6 = DAL.PA31;
+    export const PIN_D7 = DAL.PA17; //SS
+    export const PIN_LED = DAL.PA14;
+    export const PIN_D13 = DAL.PA14;
+
+    export const PIN_RXLED = DAL.PA10;
+    export const PIN_TXLED = DAL.PA15; 
+    
+    // Module RF95 LoRa
+    export const PIN_MISO = DAL.PA22;
+    export const PIN_MOSI = DAL.PA18;
+    export const PIN_SCK = DAL.PA19;
+}
